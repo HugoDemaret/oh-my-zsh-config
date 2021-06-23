7857c2635bf78ef1740b8458cc02b48a2e59097c
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2021-06-23 20:45:29 +0200

    Create ohmyzsh_install.sh

---
+++ ohmyzsh_install.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cp .zshrc ~/
+
+autoload -U compinit && compinit
